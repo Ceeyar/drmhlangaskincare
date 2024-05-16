1b7cc1e0e23f1904515032c5b9550712bb00bfde
Siya Songo <memyselfa4@gmail.com>
2024-05-16 13:17:33 +0200

    Created Footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import Typography from '../typography/Typography';
+import 'assets/styles/Styles.scss';
+
+export default function Footer() {
+
+    return (
+        <div className='footer'>
+            <Typography varient='body1' color={"white"} copy={"Copyright © 2024 Mhlanga SkinCare"} />
+        </div>
+    )
+
+}
