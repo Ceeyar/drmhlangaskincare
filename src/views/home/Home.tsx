cd385d0729c337f96ef3c1fd65cb370f46b8c463
Siya Songo <memyselfa4@gmail.com>
2024-05-16 13:18:32 +0200

    Created Proud Member component

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import './Home.scss';
+import { createStyles } from '@mui/material';
+import { ProudMember, Typography } from 'components/Index';
 
 export default function Home() {
 
+    const welcome = createStyles({
+        fontWeight: 'lighter',
+        padding: '10px'
+    })
+    const highlight = createStyles({
+        fontWeight: 'bolder',
+        padding: '0 10px',
+        fontFamily: 'monospace',
+        fontSize: '60px'
+    })
     return (
 
-        <div >
-            Home
+        <div className='container'>
+            <div className='background-image-container'>
+                <div className='text-parent'>
+                    <Typography varient="h6" style={welcome} copy={"WELCOME TO MHLANGA SKINCARE"} />
+                    <Typography varient="h3" style={highlight} copy={"Your Healthy"} />
+                    <Typography varient="h3" style={highlight} copy={"Skin Journey"} />
+                    <Typography varient="h3" color={"white"} style={highlight} copy={"Begins Here"} />
+                </div>
+            </div>
+            <ProudMember />
         </div>
 
     )
