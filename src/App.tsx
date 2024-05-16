9e607cfe45514c262e34c3748c64a03cf166e849
Siya Songo <memyselfa4@gmail.com>
2024-05-16 13:17:12 +0200

    Added MenuItems in the constants file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Screens from 'navigation/Screens';
 import './App.scss';
 import Header from './components/header/Header';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Footer from 'components/footer/Footer';
+import { Box } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header />
+        <Router>
+          <Header />
+          <Screens />
+        </Router>
       </header>
+      <Footer />
     </div>
   );
 }
---
+++ src/constants/Index.ts
@@ -0,0 +1,28 @@
+export const menuItems = [
+
+    {
+        id: 0,
+        name: "Home",
+        route: '/'
+    },
+    {
+        id: 1,
+        name: "About Us",
+        route: '/about'
+    },
+    {
+        id: 2,
+        name: "Treatment",
+        route: '/treatment'
+    },
+    {
+        id: 3,
+        name: "Contact",
+        route: '/contact'
+    },
+    {
+        id: 4,
+        name: "Appointment",
+        route: '/appointment'
+    },
+]
