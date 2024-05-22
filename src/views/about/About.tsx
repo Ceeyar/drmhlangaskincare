b698bda3e3978d6fb4d1192189da09eb352f3553
Siya Songo <memyselfa4@gmail.com>
2024-05-22 18:06:19 +0200

    Created about page

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -1,11 +1,67 @@
 import React from 'react';
+import './About.scss';
+import { Timeline, Typography } from 'components/Index';
+import { Box, createStyles } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function About() {
+    const highlight = createStyles({
+        fontWeight: 'bolder',
+        padding: '20px',
+        fontFamily: 'monospace',
+        fontSize: '60px',
+    })
+    const about = createStyles({
+        fontWeight: 'bolder',
+        padding: '0 20px',
+        fontSize: 15,
+        fontFamily: 'monospace',
+    })
+    const smallText = createStyles({
+        fontFamily: 'monospace',
+    })
 
     return (
 
-        <div >
-            About
+        <div className='container'>
+            <div className='background-image-about-container'>
+                <div className='text-parent'>
+                    <Typography color={"white"} variant="h3" style={highlight} copy={"Dr. T.S Mhlanga"} />
+                    <Typography color={"white"} variant="h6" style={about} copy={"Specialist Dermatologist"} />
+                    <Typography color={"white"} variant="h6" style={about} copy={"FCDerm (SA) Mmed (UKZN) MBChB (UCT)"} />
+                    <Typography color={"white"} variant="h6" style={about} copy={`Life Coach & Inspirational Speaker.`} />
+                </div>
+            </div>
+            <Box py={2}>
+                <Typography variant="caption" style={about} copy={`Dr. Sthembiso Mhlanga is a qualified Specialist dermatologist and Director of Mhlanga Skincare Inc. a dermatology clinic established in 2023.`} />
+            </Box>
+            <Box px={30}>
+                <Typography variant="caption" style={smallText} copy={`He is also passionate about research, as evidenced by his involvement with the Matlosana-Perinatal HIV Research Unit (PHRU), located at Tshepong Hospital (Northwest), and currently with the University of KwaZulu-Natal.`} />
+                <Typography variant="caption" style={smallText} copy={`He presented his ground-breaking research on common skin conditions in rural South Africa at the World Congress of Dermatology in Singapore. He has also presented at the National Congress of Dermatology.`} />
+                <Typography variant="caption" style={smallText} copy={`Dr. Mhlanga is also avid about development and community building; during the internship, he established Maximum Out Put Foundation, a Non-Profit Organisation. A vehicle that he used to inspire and mentor students from high school who aspired to become medical doctors.`} />
+                <Typography variant="caption" style={smallText} copy={`He is currently one of the founding members of MR (Mhlanga-Rikhotso) Foundation an NPO that mentors students from impoverished backgrounds guiding them to University and creating Job-shadowing opportunities for them.`} />
+            </Box>
+            <div>
+                <Timeline />
+            </div>
+            <div className='about-content'>
+                <Box>
+                    <Typography variant="subtitle1" style={about} copy={`Reach Us`} />
+                    <Typography variant="caption" style={about} copy={`Unit 13 Vista Fairview, 90 Main Road, Empangeni, 3880`} />
+                </Box>
+                <div>
+                    <Typography variant="h6" style={about} copy={`Follow Us`} />
+                    <FacebookIcon />
+                    <InstagramIcon />
+                    <LinkedInIcon />
+                </div>
+                <Box>
+                    <Typography variant="subtitle1" style={about} copy={`Open Hours`} />
+                    <Typography variant="caption" style={about} copy={`Mon-Fri 08:30 - 17:00\nSat-Sun 10:00 - 13:00`} />
+                </Box>
+            </div>
         </div>
 
     )
